be129f4d4822087f396e3500beebc61b1639d31f
Anirudh938 <revallianirudh@gmail.com>
2025-07-31 15:20:20 -0400

    first commit

---
+++ Kambaz/Database/index.js
@@ -0,0 +1,6 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./Assignments.js";
+import enrollments from "./enrollments.js";
+import users from "./users.js"
+export default { courses, modules, assignments, enrollments, users };

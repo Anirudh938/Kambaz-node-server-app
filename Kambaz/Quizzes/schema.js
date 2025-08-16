70a6331ffc9b876ea23b35bdcb23ae83fdc8a859
LZC173 <chen.liangz@northeastern.edu>
2025-08-17 06:59:08 +0800

    scheme fixed

--- Kambaz/Quizzes/schema.js
+++ Kambaz/Quizzes/schema.js
@@ -78,14 +78,11 @@ const schema = new mongoose.Schema({
                     type: Boolean,
                     required: false,
                     default: false
-<<<<<<< HEAD
                 },
-                webCamRequired: {
+            webCamRequired: {
                     type: Boolean,
                     required: false,
                     default: false
-=======
->>>>>>> 20a6882cc355396f9e3e198c4419598a77ecd358
                 }
         },
         assign: {
@@ -135,139 +132,3 @@ const schema = new mongoose.Schema({
 });
 
 export default schema;
-
-
-
-
-
-// ,
-//                 webCamRequired: {
-//                     type: Boolean,  
-//         }
-
-
-
-// import mongoose from "mongoose";
-// const schema = new mongoose.Schema({
-//     _id: {
-//         type: String,
-//         required: true
-//     },
-//     courseId: {
-//         type: String,
-//         ref: "CourseModel",
-//         required: true
-//     },
-//     published: {
-//         type: Boolean,
-//         required: true,
-//         default: false
-//     },
-//     details: {
-//         title: {
-//             type: String,
-//             required: true
-//         },
-//         description: {
-//             type: String,
-//             default: ""
-//         },
-//         dates: {
-//             availableFrom: {
-//                 type: String,
-//                 required: true
-//             },
-//             availableUntil: {
-//                 type: Date,
-//                 required: false
-//             },
-//             dueDate: {
-//                 type: String,
-//                 required: true
-//             }
-//         },
-//         quizType: {
-//             type: String,
-//             required: true,
-//             default: "Graded Quiz"
-//         },
-//         assignmentGroup : {
-//             type: String,
-//             required: true,
-//             default: "ASSIGNMENTS"
-//         },
-//         options: {
-//            shuffleAnswers: {
-//                type: Boolean,
-//                required: false,
-//                default: false
-//            },
-//             noOfAttempts: {
-//                type: Number,
-//                required: false,
-//                default: 1
-//             },
-//             timeLimit: {
-//                type: Number,
-//                 required: false,
-//                 default: 20
-//             },
-//             questionLocked: {
-//                 type: Boolean,
-//                 required: false,
-//                 default: false
-//             },
-//             showAnswers: {
-//                type: String,
-//                 required: false,
-//                 default: null
-//             },
-//              accessCode:    { type: String,  required: false, default: ""    } 
-//         },
-//         assign: {
-//             type: String,
-//             required: false,
-//         }
-//     },
-//     questions: [{
-//         questionId: {
-//             type: String,
-//             required: true
-//         },
-//         questionTitle: {
-//             type: String,
-//             required: true
-//         },
-//         questionDescription: {
-//             type: String,
-//             required: true
-//         },
-//         questionType: {
-//             type: String,
-//             required: true,
-//             enum: ['multi-select', 'true-false', 'fill-in-blank']
-//         },
-//         possibleAnswers: [{
-//             type: String,
-//             required: true
-//         }],
-//         correctAnswers: {
-//             type: String,
-//             required: true
-//         },
-//         isPublished: {
-//             type: Boolean,
-//             required: true,
-//             default: false
-//         },
-//         points: {
-//             type: Number,
-//             required: true,
-//             min: 0
-//         }
-//     }]
-// }, {
-//     collection: 'quizzes'
-// });
-
-// export default schema;

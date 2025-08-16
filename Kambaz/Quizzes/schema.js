4b2b0e76394b8082e3f6a3fccbaf363d72ba3781
ninaghar23 <gharachorloo.ni@northeastern.edu>
2025-08-16 17:36:51 -0400

    Update schema.js

--- Kambaz/Quizzes/schema.js
+++ Kambaz/Quizzes/schema.js
@@ -73,7 +73,17 @@ const schema = new mongoose.Schema({
                 required: false,
                 default: null
             },
-             accessCode:    { type: String,  required: false, default: ""    } 
+             accessCode:    { type: String,  required: false, default: ""    } ,
+               oneQuestionAtATime: {
+                    type: Boolean,
+                    required: false,
+                    default: false
+                },
+                webCamRequired: {
+                    type: Boolean,
+                    required: false,
+                    default: false
+                }
         },
         assign: {
             type: String,
@@ -122,3 +132,139 @@ const schema = new mongoose.Schema({
 });
 
 export default schema;
+
+
+
+
+
+// ,
+//                 webCamRequired: {
+//                     type: Boolean,  
+//         }
+
+
+
+// import mongoose from "mongoose";
+// const schema = new mongoose.Schema({
+//     _id: {
+//         type: String,
+//         required: true
+//     },
+//     courseId: {
+//         type: String,
+//         ref: "CourseModel",
+//         required: true
+//     },
+//     published: {
+//         type: Boolean,
+//         required: true,
+//         default: false
+//     },
+//     details: {
+//         title: {
+//             type: String,
+//             required: true
+//         },
+//         description: {
+//             type: String,
+//             default: ""
+//         },
+//         dates: {
+//             availableFrom: {
+//                 type: String,
+//                 required: true
+//             },
+//             availableUntil: {
+//                 type: Date,
+//                 required: false
+//             },
+//             dueDate: {
+//                 type: String,
+//                 required: true
+//             }
+//         },
+//         quizType: {
+//             type: String,
+//             required: true,
+//             default: "Graded Quiz"
+//         },
+//         assignmentGroup : {
+//             type: String,
+//             required: true,
+//             default: "ASSIGNMENTS"
+//         },
+//         options: {
+//            shuffleAnswers: {
+//                type: Boolean,
+//                required: false,
+//                default: false
+//            },
+//             noOfAttempts: {
+//                type: Number,
+//                required: false,
+//                default: 1
+//             },
+//             timeLimit: {
+//                type: Number,
+//                 required: false,
+//                 default: 20
+//             },
+//             questionLocked: {
+//                 type: Boolean,
+//                 required: false,
+//                 default: false
+//             },
+//             showAnswers: {
+//                type: String,
+//                 required: false,
+//                 default: null
+//             },
+//              accessCode:    { type: String,  required: false, default: ""    } 
+//         },
+//         assign: {
+//             type: String,
+//             required: false,
+//         }
+//     },
+//     questions: [{
+//         questionId: {
+//             type: String,
+//             required: true
+//         },
+//         questionTitle: {
+//             type: String,
+//             required: true
+//         },
+//         questionDescription: {
+//             type: String,
+//             required: true
+//         },
+//         questionType: {
+//             type: String,
+//             required: true,
+//             enum: ['multi-select', 'true-false', 'fill-in-blank']
+//         },
+//         possibleAnswers: [{
+//             type: String,
+//             required: true
+//         }],
+//         correctAnswers: {
+//             type: String,
+//             required: true
+//         },
+//         isPublished: {
+//             type: Boolean,
+//             required: true,
+//             default: false
+//         },
+//         points: {
+//             type: Number,
+//             required: true,
+//             min: 0
+//         }
+//     }]
+// }, {
+//     collection: 'quizzes'
+// });
+
+// export default schema;

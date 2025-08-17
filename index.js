cbfd2b9b02c7673b56bf99217753d25e13ef3cef
Anirudh938 <revallianirudh@gmail.com>
2025-08-17 17:44:38 -0400

    string

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import QuizzesRoutes from "./Kambaz/Quizzes/routes.js";
 import QuizAttemptRoutes from "./Kambaz/QuizAttempts/routes.js";
 
 const CONNECTION_STRING =  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/Kambaz"
+console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
